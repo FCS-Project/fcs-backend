b7615380a4606573d50c179f1c1d477598a2a3a5
aryanteng <aryanteng892@gmail.com>
2022-10-03 22:38:37 +0530

    testing

--- script.ts
+++ script.ts
@@ -3,28 +3,28 @@ const prisma = new PrismaClient();
 
 async function main() {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const bcrypt = require('bcrypt');
-  const saltRounds = 12;
-  const dto = {
-    name: 'Aryan Teng',
-    email: 'aryanteng892@gmail.com',
-    password: '1234',
-  };
-  const hash = await bcrypt.hash(dto.password, saltRounds);
-  dto.password = hash;
-  console.log(dto);
-  const check = await bcrypt.compare('1234', dto.password);
-  console.log(check);
-
-  // result == false
-
+  //   const bcrypt = require('bcrypt');
+  //   const saltRounds = 12;
+  //   const dto = {
+  //     name: 'Aryan Teng',
+  //     email: 'aryanteng892@gmail.com',
+  //     password: '1234',
+  //   };
+  //   const hash = await bcrypt.hash(dto.password, saltRounds);
+  //   dto.password = hash;
   //   console.log(dto);
-
+  //   const check = await bcrypt.compare('1234', dto.password);
+  //   console.log(check);
+  // result == false
+  //   console.log(dto);
   //   const user = await prisma.user.create({
   //     data: dto,
   //   });
-
   //   console.log(user);
+  //   const doc = await prisma.document.create({
+  //     data: { userId: '7b1ade90-080d-476e-b4aa-dd9c36d3891a' },
+  //   });
+  //   console.log(doc);
 }
 
 main()
