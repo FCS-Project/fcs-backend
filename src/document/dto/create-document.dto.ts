8d8f2b39e3029af2be524eaf5dc7a76be3cf813c
aryanteng <aryanteng892@gmail.com>
2022-10-03 22:46:02 +0530

    create document dto

--- src/document/dto/create-document.dto.ts
+++ src/document/dto/create-document.dto.ts
@@ -1,11 +1,7 @@
-import { User } from '@prisma/client';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateDocumentDto {
   @IsString()
   @IsNotEmpty()
   userId: string;
-
-  //   @IsNotEmpty()
-  //   user: User;
 }
