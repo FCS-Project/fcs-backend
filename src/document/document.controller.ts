74ca337163e3467bac298580b980d7af9f7ff283
aryanteng <aryanteng892@gmail.com>
2022-10-04 18:07:20 +0530

    document service

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { CreateDocumentDto } from './dto/create-document.dto';
-import { UpdateDocumentDto } from './dto/update-document.dto';
 
 @Controller('document')
 export class DocumentController {
@@ -28,14 +19,6 @@ export class DocumentController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.documentService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateDocumentDto: UpdateDocumentDto,
-  ) {
-    return this.documentService.update(+id, updateDocumentDto);
   }
 
   @Delete(':id')
--- src/document/document.service.ts
+++ src/document/document.service.ts
@@ -1,7 +1,6 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateDocumentDto } from './dto/create-document.dto';
-import { UpdateDocumentDto } from './dto/update-document.dto';
 
 @Injectable()
 export class DocumentService {
@@ -10,7 +9,7 @@ export class DocumentService {
     try {
       return await this.prisma.document.create({ data: createDocumentDto });
     } catch (error) {
-      return { success: false, message: error.message };
+      throw new HttpException(error, 500);
     }
   }
 
@@ -26,17 +25,13 @@ export class DocumentService {
     }
   }
 
-  update(id: number, updateDocumentDto: UpdateDocumentDto) {
-    return `This action updates a #${id} document`;
-  }
-
   async remove(id: string) {
     try {
       const document = await this.prisma.document.delete({ where: { id } });
       if (document) {
         return { success: true };
       } else {
-        return { success: false, message: 'This document does not exist' };
+        throw new BadRequestException('Document does not exist!');
       }
     } catch (error) {
       return new HttpException(error, 500);
