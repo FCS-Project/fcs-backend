213e0df295447520d2f32492f8ecb3ad17b3f334
aryanteng <aryanteng892@gmail.com>
2022-10-04 16:06:29 +0530

    success parameter in update user api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,10 +12,11 @@ export class UserService {
 
   async update(id: string, updateUserDto: UpdateUserDto) {
     try {
-      return await this.prisma.user.update({
+      const updatedData = await this.prisma.user.update({
         where: { id },
         data: updateUserDto,
       });
+      return { success: true, data: updatedData };
     } catch (error) {
       throw new HttpException(error, 500);
     }
