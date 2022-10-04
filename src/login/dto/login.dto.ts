e5e315e8aafb0389d77305525981da239c32769e
aryanteng <aryanteng892@gmail.com>
2022-10-04 14:25:39 +0530

    login dto

--- src/login/dto/login.dto.ts
+++ src/login/dto/login.dto.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsString()
   @IsOptional()
+  @IsEmail()
   email: string;
 
   @IsString()
