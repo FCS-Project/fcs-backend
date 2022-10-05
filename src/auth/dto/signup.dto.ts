b28e189ea21369b36ddcdb0d92c19790d2ec227b
aryanteng <aryanteng892@gmail.com>
2022-10-05 15:05:31 +0530

    signup dto documentation

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,25 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class SignUpDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsString()
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ required: false })
   mobileNumber: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ required: false })
   role: Role;
 }
