4233f32b0a0c30f1ca26292603dd95fa77f63a2b
aryanteng <aryanteng892@gmail.com>
2022-10-22 22:47:22 +0530

    auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './roles.guard';
+import { AtGuard } from './common/guards';
 
 @Module({
   controllers: [AppController],
@@ -15,6 +16,10 @@ import { RolesGuard } from './roles.guard';
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
     },
   ],
   imports: [UserModule, DocumentModule, PrismaModule, AuthModule],
