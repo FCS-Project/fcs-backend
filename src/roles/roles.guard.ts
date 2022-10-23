9ed95395a1dcc05ead92b37d3c5dab37cbd9e1cb
aryanteng <aryanteng892@gmail.com>
2022-10-23 14:56:37 +0530

    working on rbac

--- src/roles/roles.guard.ts
+++ src/roles/roles.guard.ts
@@ -1,21 +1,46 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { Role } from '@prisma/client';
-import { ROLES_KEY } from './roles.decorator';
+// import {
+//   Injectable,
+//   CanActivate,
+//   ExecutionContext,
+//   Inject,
+//   forwardRef,
+// } from '@nestjs/common';
+// import { Reflector } from '@nestjs/core';
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs/operators';
+// import { User } from 'src/user/entities/user.entity';
+// import { UserService } from 'src/user/user.service';
 
-@Injectable()
-export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+// @Injectable()
+// export class RolesGuard implements CanActivate {
+//   constructor(
+//     private reflector: Reflector,
 
-  canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (!requiredRoles) {
-      return true;
-    }
-    const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
-  }
-}
+//     @Inject(forwardRef(() => UserService))
+//     private userService: UserService,
+//   ) {}
+
+//   canActivate(
+//     context: ExecutionContext,
+//   ): boolean | Promise<boolean> | Observable<boolean> {
+//     const roles = this.reflector.get<string[]>('roles', context.getHandler());
+//     if (!roles) {
+//       return true;
+//     }
+
+//     const request = context.switchToHttp().getRequest();
+//     const user: User = request.user;
+
+//     return this.userService.findOne(user.id).pipe(
+//       map((user: User) => {
+//         const hasRole = () => roles.indexOf(user.roles) > -1;
+//         let hasPermission = false;
+
+//         if (hasRole()) {
+//           hasPermission = true;
+//         }
+//         return user && hasPermission;
+//       }),
+//     );
+//   }
+// }
