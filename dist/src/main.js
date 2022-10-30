72f7d2d60d7feaccd183a898919b57fd0d43824a
aryanteng <aryanteng892@gmail.com>
2022-10-30 16:38:22 +0530

    main.ts body-parser

--- dist/src/main.js
+++ dist/src/main.js
@@ -4,9 +4,12 @@ const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
+const bodyParser = require("body-parser");
 const helmet_1 = require("helmet");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true }));
     app.use((0, helmet_1.default)());
     const config = new swagger_1.DocumentBuilder()
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(helmet());
   const config = new DocumentBuilder()
