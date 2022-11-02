ec7ef38d359cafac494ff331c97b1e7bdf17397b
aryanteng <aryanteng892@gmail.com>
2022-11-02 14:00:57 +0530

    update product not required

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
@@ -14,10 +13,6 @@ export class ProductService {
 
   findOne(id: number) {
     return `This action returns a #${id} product`;
-  }
-
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
   }
 
   remove(id: number) {
