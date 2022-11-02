b092ea4924f5f1e092437236c84d257e2ce2d6de
aryanteng <aryanteng892@gmail.com>
2022-11-02 14:05:30 +0530

    create product dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,24 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  imgSrc: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  price: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  userId: string;
+}
