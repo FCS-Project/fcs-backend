468b1e786a4c1e98e29b464c9467733a78072a6e
aryanteng <aryanteng892@gmail.com>
2022-11-26 03:13:27 +0530

    order resource

--- dist/src/order/dto/create-order.dto.d.ts
+++ dist/src/order/dto/create-order.dto.d.ts
@@ -1,2 +1,4 @@
 export declare class CreateOrderDto {
+    amount: string;
+    productId: string;
 }
--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,14 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  amount: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  productId: string;
+}
